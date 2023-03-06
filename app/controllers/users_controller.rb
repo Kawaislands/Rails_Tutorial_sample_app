2db606126c9d848073c7031322a807f67fb74115
Kawaislands <kawaislnads@gmail.com>
2023-03-06 01:46:01 +0000

    create action時のストロングパラメータの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,24 @@
 class UsersController < ApplicationController
-  # GET /users/:id
+  
   def show
+    # GET /users/:id
     @user = User.find(params[:id])
   end
 
   def new
+    # GET /users/new
+    @user = User.new
   end
+  
+  def create
+    # POST /users + (params)
+    User.create(user_params)
+  end
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
