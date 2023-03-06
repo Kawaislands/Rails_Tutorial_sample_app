0ff053c7eb96173c8c258b938b167708eb8433f3
Kawaislands <kawaislnads@gmail.com>
2023-03-06 06:32:23 +0000

    SessionsHelperでメソッドを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
+  # GET /login
   def new
+    # x @session = Session.new
+    # o scope: :session + url: login_path
+  end
+  
+  # POST /login
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 end
