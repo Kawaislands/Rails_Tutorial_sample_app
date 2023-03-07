7fc1acad151abec29548b8e039bfcba74c7f7fbf
Kawaislands <kawaislnads@gmail.com>
2023-03-07 01:44:13 +0000

    forgetメソッドの実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,17 +23,20 @@ module SessionsHelper
     end
   end
   
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
-  end
-  
   def logged_in?
     !current_user.nil?
   end
   
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # 現在のユーザーをログアウトする
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
