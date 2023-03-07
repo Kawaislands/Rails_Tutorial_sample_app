b9520c590cfc04d44d22828b375455a62f02fb61
Kawaislands <kawaislnads@gmail.com>
2023-03-07 01:31:14 +0000

    remember_meの実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       log_in user
+      remember_user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
