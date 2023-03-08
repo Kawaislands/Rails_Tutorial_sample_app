9f67850980e8342158b8d522871f1fe9999fd5f6
Kawaislands <kawaislnads@gmail.com>
2023-03-08 02:01:10 +0000

    プレビューの実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  host = "a31c366db0e84f33b993dcf43a889410.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
