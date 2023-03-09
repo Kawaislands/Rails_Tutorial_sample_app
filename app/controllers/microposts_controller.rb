27a20fe1a4a277deaa5a03c2986a6ecde7446deb
Kawaislands <kawaislnads@gmail.com>
2023-03-09 07:33:14 +0000

    create destroy actionの追加

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,7 +1,9 @@
 class MicropostsController < ApplicationController
-    def create
-    end
-    
-    def destroy
-    end
+  before_action :logged_in_user, only: [:create, :destroy]
+
+  def create
+  end
+
+  def destroy
+  end
 end
